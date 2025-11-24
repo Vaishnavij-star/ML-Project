4163e94b63e47f726dd214ad94256524da16fe67
Vaishnavi jaiswal <gungunjaiswal358@gmail.com>
2025-11-24 14:00:46 +0530

    Update model trainer, utils, and project structure

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,46 @@
 import os
 import sys
 import dill
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
+
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
+
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
 
-def load_object(file_path):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
-        with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
+        report = {}
+
+        for model_name, model in models.items():
+            print(f"🔍 Tuning {model_name}...")
+
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_pred = model.predict(X_test)
+            score = model.score(X_test, y_test)
+
+            report[model_name] = score
+
+        return report
+
     except Exception as e:
         raise CustomException(e, sys)
